946b7b27732fb03da89b5f4fb540c97c23bde5fc
BradFernando <bfcorro@espe.edu.ec>
2024-07-17 22:21:15 -0500

    Corrección de errores ExportToPDF

--- components/report/ExportToPdf.tsx
+++ components/report/ExportToPdf.tsx
@@ -1,20 +1,23 @@
-"use client"
+"use client";
 
-import { useEffect } from 'react';
 import { jsPDF } from 'jspdf';
 import 'jspdf-autotable';
-import { FaFilePdf } from 'react-icons/fa'; // Importar el ícono de PDF
+import { FaFilePdf } from 'react-icons/fa';
+import React from "react";
 
+// Definición de tipos para los productos
 interface Product {
   name: string;
   category: { name: string };
   price: number;
 }
 
+// Propiedades del componente ExportToPdf
 interface ExportToPdfProps {
   data: Product[];
 }
 
+// Componente principal ExportToPdf
 const ExportToPdf: React.FC<ExportToPdfProps> = ({ data }) => {
   const exportToPdf = () => {
     if (!data) return;
@@ -22,35 +25,31 @@ const ExportToPdf: React.FC<ExportToPdfProps> = ({ data }) => {
     const products = data.map((product: Product, index: number) => ({
       N: index + 1,
       Producto: product.name,
-      Categoría: product.category.name,
+      Categoria: product.category.name,  // Cambio de Categoría a Categoria
       Precio: product.price.toFixed(2),
     }));
 
     const total = products.reduce((acc: number, curr: { Precio: string }) => acc + parseFloat(curr.Precio), 0).toFixed(2);
-
-    const productsWithTotal = [...products, { N: '', Producto: '', Categoría: 'Total:', Precio: total }];
+    const productsWithTotal = [...products, { N: '', Producto: '', Categoria: 'Total:', Precio: total }];
 
     const doc = new jsPDF();
 
     // Agregar el logo del restaurante como encabezado en las esquinas
-    const logoWidth = 20; // Tamaño reducido del logo
-    const logoHeight = 20; // Tamaño reducido del logo
+    const logoWidth = 20;
+    const logoHeight = 20;
     const pageWidth = doc.internal.pageSize.getWidth();
     const logoMargin = 6;
-    
-    // Logo en la esquina superior izquierda
+
     const logoLeft = logoMargin;
     const logoTop = logoMargin;
     const logo1 = new Image();
-    logo1.src = '/logo-prototipo.png'; // Ruta relativa al archivo en la carpeta public
+    logo1.src = '/logo-prototipo.png';
     doc.addImage(logo1, 'PNG', logoLeft, logoTop, logoWidth, logoHeight);
 
-    // Logo en la esquina superior derecha
     const logo2 = new Image();
-    logo2.src = '/logo-prototipo.png'; // Ruta relativa al archivo en la carpeta public
+    logo2.src = '/logo-prototipo.png';
     const logo2Left = pageWidth - logoMargin - logoWidth;
-    const logo2Top = logoMargin;
-    doc.addImage(logo2, 'PNG', logo2Left, logo2Top, logoWidth, logoHeight);
+    doc.addImage(logo2, 'PNG', logo2Left, logoMargin, logoWidth, logoHeight);
 
     // Configurar estilos para el título
     doc.setFont('Arial', 'bold');
@@ -59,34 +58,33 @@ const ExportToPdf: React.FC<ExportToPdfProps> = ({ data }) => {
     doc.text('Listado de productos', doc.internal.pageSize.width / 2, 15, { align: 'center' });
 
     // Agregar el texto "Inventario de la inversión de los productos" sin negrita
-  doc.setFontSize(11); // Tamaño de letra para el texto adicional
-  doc.setFont('Arial', 'normal'); // Establecer la fuente normal
-  doc.text('Inventario de la inversión de los productos', doc.internal.pageSize.width / 2, 22, { align: 'center' });
-
+    doc.setFontSize(11);
+    doc.setFont('Arial', 'normal');
+    doc.text('Inventario de la inversión de los productos', doc.internal.pageSize.width / 2, 22, { align: 'center' });
 
     // Configuración de estilos para la tabla
     const tableStyles = {
-      startY: logoTop + logoHeight + logoMargin, // Iniciar después de los logos y el título
+      startY: logoTop + logoHeight + logoMargin,
       headStyles: { fillColor: [41, 128, 185], textColor: [255, 255, 255], fontStyle: 'bold' },
       bodyStyles: { textColor: 50 },
       alternateRowStyles: { fillColor: [245, 245, 245] },
       theme: 'grid',
-      tableWidth: 'auto', // Ajustar automáticamente el ancho de la tabla
+      tableWidth: 'auto',
     };
 
     // Usar jspdf-autotable para generar la tabla
     (doc as any).autoTable({
-      head: [['N°', 'Producto', 'Categoría', 'Precio']],
+      head: [['N°', 'Producto', 'Categoria', 'Precio']],
       body: productsWithTotal.map((p, index) => {
         if (index === productsWithTotal.length - 1) {
           return [
             { content: p.N, styles: { cellWidth: 'wrap', fontStyle: 'bold', halign: 'center', fillColor: [173, 216, 230] } },
             { content: p.Producto, styles: { cellWidth: 'wrap', fontStyle: 'bold', halign: 'center', fillColor: [173, 216, 230] } },
-            { content: p.Categoría, styles: { cellWidth: 'wrap', fontStyle: 'bold', halign: 'center', fillColor: [173, 216, 230] } },
+            { content: p.Categoria, styles: { cellWidth: 'wrap', fontStyle: 'bold', halign: 'center', fillColor: [173, 216, 230] } },
             { content: p.Precio, styles: { cellWidth: 'wrap', fontStyle: 'bold', halign: 'center', fillColor: [173, 216, 230] } }
           ];
         }
-        return [p.N, p.Producto, p.Categoría, p.Precio];
+        return [p.N, p.Producto, p.Categoria, p.Precio];
       }),
       ...tableStyles,
     });
@@ -95,12 +93,12 @@ const ExportToPdf: React.FC<ExportToPdfProps> = ({ data }) => {
   };
 
   return (
-    <button
-      onClick={exportToPdf}
-      className="py-2 px-4 bg-blue-500 text-white rounded flex items-center hover:bg-red-500"
-    >
-       <FaFilePdf style={{ fontSize: '24px', color: 'red', marginRight: '8px' }} /> Exportar a PDF
-    </button>
+      <button
+          onClick={exportToPdf}
+          className="py-2 px-4 bg-blue-500 text-white rounded flex items-center hover:bg-red-500"
+      >
+        <FaFilePdf style={{ fontSize: '24px', color: 'red', marginRight: '8px' }} /> Exportar a PDF
+      </button>
   );
 };
 
