eb64ff6910182444009e6ec3e9756c9a123bb187
BradFernando <bfcorro@espe.edu.ec>
2024-07-17 22:32:10 -0500

    Corrección de errores en UI en CategoryIcon

--- components/ui/CategoryIcon.tsx
+++ components/ui/CategoryIcon.tsx
@@ -1,40 +1,32 @@
-"use client"
+"use client";
 
 import { Category } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
 import { useParams } from "next/navigation";
 
-
 type CategoryIconProps = {
     category: Category;
-    };
+};
 
+export default function CategoryIcon({ category }: CategoryIconProps) {
+    const params = useParams<{ category: string }>();
 
-export default function CategoryIcon({category} : CategoryIconProps ) {
-  const params = useParams<{category : string}>()
-  
+    return (
+        <div
+            className={`${
+                params && category.slug === params.category
+                    ? "bg-sky-900 hover:bg-indigo-800"
+                    : ""
+            } flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b`}
+        >
+            <div className="w-16 h-16 relative">
+                <Image src={`/icon_${category.slug}.png`} alt="Imagen Categoria" fill />
+            </div>
 
-  return (
-    <div 
-        className={`${category.slug === params.category ? 'bg-sky-900 hover:bg-indigo-800' : ''} flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b`}
-    >
-        <div className="w-16 h-16 relative ">
-        <Image 
-        src={`/icon_${category.slug}.png`} 
-        alt="Imagen Categoria"
-        fill
-        />
+            <Link className="text-xl font-bold" href={`/order/${category.slug}`}>
+                {category.name}
+            </Link>
         </div>
-
-        <Link 
-            className="text-xl font-bold"
-            href={`/order/${category.slug}`}
-            >
-            {category.name}
-        </Link>
-
-    </div>
-  )
+    );
 }
-
