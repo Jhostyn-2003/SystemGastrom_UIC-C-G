dd50b82dcce50319e35c1df65681d971b12e2acd
Jhostyn-2003 <jhostyngavilanez@gmail.com>
2024-08-21 02:56:57 -0500

    Correcion de despliegue v2

--- pages/api/revenue/[period].ts
+++ pages/api/revenue/[period].ts
@@ -1,12 +1,18 @@
 import { prisma } from '@/src/lib/prisma';
 import { NextApiRequest, NextApiResponse } from 'next';
 
+// Define el tipo de los datos de ingresos
+type RevenueData = {
+  date: Date;
+  total: number;
+}[];
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { period } = req.query;
   const { startDate, endDate } = req.query;
 
   try {
-    let revenueData;
+    let revenueData: RevenueData = [];
 
     if (period === 'daily') {
       if (!startDate || !endDate) {
@@ -23,7 +29,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       // Ajusta endDate para incluir todo el día
       end.setHours(23, 59, 59, 999);
 
-      revenueData = await prisma.$queryRaw`
+      revenueData = await prisma.$queryRaw<RevenueData>`
         SELECT
           DATE_TRUNC('day', "date") AS date,
           SUM("total") AS total
@@ -33,7 +39,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         ORDER BY date;
       `;
     } else if (period === 'monthly') {
-      revenueData = await prisma.$queryRaw`
+      revenueData = await prisma.$queryRaw<RevenueData>`
         SELECT
           DATE_TRUNC('month', "date") AS date,
           SUM("total") AS total
@@ -47,16 +53,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     }
 
     // Formatear la fecha en el formato YYYY-MM-DD
-    const formattedRevenueData = revenueData.map((entry: { date: Date, total: number }) => ({
+    const formattedRevenueData = revenueData.map((entry) => ({
       date: entry.date.toISOString().split('T')[0],
-      total: entry.total
+      total: entry.total,
     }));
-
-    //console.log('Revenue Data:', formattedRevenueData);
 
     res.status(200).json({ [`${period}Revenue`]: formattedRevenueData });
   } catch (error) {
-   // console.error('Error fetching revenue data:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 }
