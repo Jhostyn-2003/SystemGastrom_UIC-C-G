67a68bc8f337f5cb1954ba52eb00725407070e5e
Jhostyn-2003 <jhostyngavilanez@gmail.com>
2024-08-21 02:46:24 -0500

    Correcion de despliegue v1

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,22 +3,6 @@ import { PrismaClient } from '@prisma/client'
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
 export const prisma =
-    globalForPrisma.prisma ||
-    new PrismaClient({
-        log: ['query'],
-    })
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
-
-
-
-
-//Este estaba Funcional primero
-/*import { PrismaClient } from '@prisma/client'
-
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
-
-export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
     log: ['query'],
@@ -26,4 +10,3 @@ export const prisma =
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
-*/
